91d2d3a95a36226f98f50c5deee2512837d36bc8
Anvarjon571 <79329183+Anvar717@users.noreply.github.com>
2023-07-10 11:22:10 +0500

    sdfsdf

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div>
+  <b-overlay>
+    <div class="mycards">
+      <div>
+        <b-card style="border: 1px solid red;" v-for="(item, index) in items" :key="index">
+          <p>{{ item.name }}</p>
+        </b-card>
+      </div>
+    </div>
+  </b-overlay>
+  <!-- <div>
     <ul>
       <li v-for="(item, index) in items" :key="index">{{ item.name }}</li>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 <script>
 import PokemonService from "@/services/poke.service";
-
+// import {
+//   BOverlay,
+//   BCard,
+//   BRow,
+//   BCol,
+// } from "bootstrap-vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    // BOverlay,
+    // BCard,
+    // BRow,
+    // BCol
+  },
+
   data() {
     return {
       items: [],
@@ -26,7 +46,18 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.mycards {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: baseline;
+  align-content: space-evenly;
+  width: 100%;
+
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import ApiService from "@/services/api.service";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 // import axios from "axios";
 
 Vue.config.productionTip = false;
 // axios.defaults.baseURL = "https://pokeapi.co/api/v2/";
-
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 ApiService.mount401Interceptor();
 new Vue({
   render: (h) => h(App),
